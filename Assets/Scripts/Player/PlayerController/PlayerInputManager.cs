b9f0bff5634a47597ea41dc2daea99b0af017b55
Sdrucito <sdrucito94@gmail.com>
2025-05-01 17:57:39 +0200

    Add InputManager for Camera and fix bug when transitioning

--- Assets/Scripts/Player/PlayerController/PlayerInputManager.cs
+++ Assets/Scripts/Player/PlayerController/PlayerInputManager.cs
@@ -13,11 +13,15 @@ namespace Player.PlayerController
         private InputAction _jumpAction;
         private InputAction _changeModeAction;
         private InputAction _sprintImpulseAction;
+        private InputAction _previousCameraAction;
+        private InputAction _nextCameraAction;
     
         public event Action<Vector2> OnMoveInput;
         public event Action OnJumpInput;
         public event Action OnModeChangeInput;
         public event Action<Vector2> OnSprintImpulseInput;
+        public event Action SetCamera1;
+        public event Action SetCamera2;
     
         private Vector2 _currentMoveInput = Vector2.zero;
         private bool _isSprintImpulse = false;
@@ -38,6 +42,8 @@ namespace Player.PlayerController
             _jumpAction = _playerInput.actions.FindAction("Jump");
             _changeModeAction = _playerInput.actions.FindAction("ChangeMode");
             _sprintImpulseAction = _playerInput.actions.FindAction("Sprint");
+            _previousCameraAction = _playerInput.actions.FindAction("Previous Camera");
+            _nextCameraAction = _playerInput.actions.FindAction("Next Camera");
         
             _moveAction.performed += ctx => _currentMoveInput = ctx.ReadValue<Vector2>();
             _moveAction.canceled += ctx => _currentMoveInput = Vector2.zero;
@@ -45,7 +51,8 @@ namespace Player.PlayerController
             _changeModeAction.started += ctx => OnModeChangeInput?.Invoke();
             _sprintImpulseAction.started += ctx => _isSprintImpulse = true;
             _sprintImpulseAction.canceled += ctx => _isSprintImpulse = false;
-
+            _previousCameraAction.started += ctx => SetCamera1?.Invoke();
+            _nextCameraAction.started += ctx => SetCamera2?.Invoke();
         }
 
         private void FixedUpdate()
