fed7e2ffca34a3961a87035400d08f362bce4dc2
Lieno <albertocag9@gmail.com>
2025-06-20 11:41:13 +0200

    Corrected turret spatialized audio

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -97,7 +97,7 @@ namespace Player
         private void OnCollisionEnter(Collision other)
         {
             // Create collision data wrapper
-            CollisionData collisionData = new CollisionData(other, other.gameObject.layer, other.gameObject.tag, Rigidbody.velocity.magnitude);
+            CollisionData collisionData = new CollisionData(other, other.gameObject.layer, other.gameObject.tag, Rigidbody.linearVelocity.magnitude);
             if (collisionData.Tag == "Ground")
             {
                 CameraShake.Instance.Shake("BallLanding");
