bd5b473cd61d37e352ccf3695ca757b4f58098a4
LienoPC <albertocag9@gmail.com>
2025-07-01 12:02:30 +0200

    Fixed leg dragging

--- Assets/Scripts/Player/Animation/PlayerKneeWalkAnimator.cs
+++ Assets/Scripts/Player/Animation/PlayerKneeWalkAnimator.cs
@@ -226,7 +226,7 @@ namespace Player.Animation
             }
             else
             {
-                if (IsStartedMoving(isMoving))
+                if (IsStartedMoving(isMoving) || (_wasMoving && isMoving && _currentGroup == StepGroup.Idle))
                 {
                     _currentGroup = StepGroup.GroupA;
                     StartStepForGroup(_currentGroup);
